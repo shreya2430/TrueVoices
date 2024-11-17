c183ad07f9e7549fab90f6bdbb761746d97b1735
Shreya Wanisha <wanisha.s@northeastern.edu>
2024-11-17 02:57:21 -0500

    add thank-you-page-router in index router

--- api/service/routers/index.js
+++ api/service/routers/index.js
@@ -1,6 +1,8 @@
 import extraSettingsRouter from './extra-settings-router.js';
+import thankYouPageRouter from './thank-you-page.router.js';
 
 const intializeRoutes = (app) => {
-    app.use('/spaces', extraSettingsRouter); 
+    app.use('/spaces', extraSettingsRouter);
+    app.use('/spaces', thankYouPageRouter);
 };
 export default intializeRoutes;
