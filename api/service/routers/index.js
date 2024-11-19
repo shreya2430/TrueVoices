367118b6aeeb98f64e18d492ad02a49cf1d42a50
maithwade25 <wade.mai@northeastern.edu>
2024-11-19 13:48:44 -0500

    Adding changes in routers/index.js

--- api/service/routers/index.js
+++ api/service/routers/index.js
@@ -1,13 +1,13 @@
 import extraSettingsRouter from './extra-settings-router.js';
 import thankYouPageRouter from './thank-you-page.router.js';
 import testimonialRoutes from "./testimonial-router.js"; // Your feature's router
-import userAuthRouter from './user-authentication-router.js';
-import spacesRouter from './spaces.js';
+import userAuthRouter from './user-authentication-router.js'
 
 const intializeRoutes = (app) => {
-    app.use('/v1/spaces', extraSettingsRouter, thankYouPageRouter, spacesRouter);
-    // app.use('/spaces', thankYouPageRouter);
-    app.use("/v1/testimonials", testimonialRoutes); // Testimonial routes
-    app.use('/v1/auth', userAuthRouter); //user-authentication router
+    app.use('/spaces', extraSettingsRouter);
+    app.use('/spaces', thankYouPageRouter);
+    app.use("/api/testimonials", testimonialRoutes); // Testimonial routes
+    app.use("/api/auth", userAuthRouter); // User authentication routes
+
 };
 export default intializeRoutes;
