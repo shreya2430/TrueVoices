71548fd7f60d43730b3246cb963165b4d5ef142a
maithwade25 <wade.mai@northeastern.edu>
2024-11-19 11:18:19 -0500

    Adding the user-authentication-router layer and adding that route to index.js

--- api/service/routers/index.js
+++ api/service/routers/index.js
@@ -1,10 +1,12 @@
 import extraSettingsRouter from './extra-settings-router.js';
 import thankYouPageRouter from './thank-you-page.router.js';
 import testimonialRoutes from "./testimonial-router.js"; // Your feature's router
+import userAuthRouter from './user-authentication-router.js';
 
 const intializeRoutes = (app) => {
     app.use('/spaces', extraSettingsRouter);
     app.use('/spaces', thankYouPageRouter);
     app.use("/api/testimonials", testimonialRoutes); // Testimonial routes
+    app.use('/auth', userAuthRouter); //user-authentication router
 };
 export default intializeRoutes;
