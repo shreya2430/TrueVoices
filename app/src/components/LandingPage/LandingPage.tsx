0e0ae8215feae3be93dc5bc08402567e8c8654c2
Shreya Wanisha <wanisha.s@northeastern.edu>
2024-11-24 07:31:21 -0500

    add all necessary components to landing page

---
+++ app/src/components/LandingPage/LandingPage.tsx
@@ -0,0 +1,20 @@
+import Header from './Header';
+import Hero from './Hero';
+import Features from './Features';
+import Team from './Team';
+import Footer from './Footer';
+
+
+const LandingPage = () => {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <Features />
+      <Team />   
+      <Footer />
+    </div>
+  );
+};
+
+export default LandingPage;
