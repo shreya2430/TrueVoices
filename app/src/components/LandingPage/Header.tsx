3422487e10ba08f3b48e5e2d718ec9ccaefc6c5c
Shreya Wanisha <wanisha.s@northeastern.edu>
2024-11-24 07:29:17 -0500

    update Header to remove unecessary comments

--- app/src/components/LandingPage/Header.tsx
+++ app/src/components/LandingPage/Header.tsx
@@ -1,5 +1,3 @@
-// import { ThemeContext } from '../ThemeContext.jsx';
-// import { Switch } from '@mui/material';
 import { AppBar, Toolbar, Typography, Button } from '@mui/material';
 
 const Header = () => {
@@ -9,13 +7,11 @@ const Header = () => {
       <Toolbar>
         <Typography variant="h6" sx={{ flexGrow: 1 }}>
           TrueVoices
-        {/* <Switch checked={theme === 'dark'} onChange={toggleTheme} /> */}
         </Typography>
         <Button color="inherit">Home</Button>
         <Button color="inherit">Features</Button>
         <Button color="inherit">Pricing</Button>
         <Button color="inherit">Sign In</Button>
-        {/* <Button color="inherit" variant="outlined">Get Started</Button> */}
       </Toolbar>
     </AppBar>
   );
