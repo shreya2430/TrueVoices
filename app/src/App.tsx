fe74f4ccc008a33558bd8243f3fde11ece676ae7
Shreya Wanisha <wanisha.s@northeastern.edu>
2024-11-27 17:51:09 -0500

    add template for dashboard

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,18 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage/LandingPage'
+import Dashboard from './components/Dashboard/Dashboard'; 
 import { SpaceForm } from './components/space/SpaceForm'
 
 function App() {
-	return (
-		<div>
-			<LandingPage />
-			<SpaceForm />
-		</div>
+  return (
+      <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/dashboard" element={<Dashboard />} /> 
+        <Route path="/space" element={<SpaceForm />} /> 
+      </Routes>
+    </Router>
 	)
 }
 
