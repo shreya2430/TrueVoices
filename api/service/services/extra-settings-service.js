bfe1ae1bce880bd13dbc76084cac17b7187fc40d
Shreya Wanisha <wanisha.s@northeastern.edu>
2024-11-27 19:46:51 -0500

    remove unnecessary comments

--- api/service/services/extra-settings-service.js
+++ api/service/services/extra-settings-service.js
@@ -7,7 +7,6 @@ export async function getSettings(spaceId) {
     const space = await Space.findOne({ spaceName: spaceId });
     if (!space) throw new NotFoundError(`The space ${spaceId} does not exist`);
     const settings = await ExtraSettings.findById(space.extraSettings);
-    // const settings = await ExtraSettings.findOne({ spaceId });
     if (!settings) throw new NotFoundError(`The space ${spaceId} does not exist`);
     return settings;
 }
@@ -17,14 +16,14 @@ export async function updateSettings(spaceId, updates) {
     const space = await Space.findOne({ spaceName: spaceId });
     if (!space) throw new NotFoundError(`The space ${spaceId} does not exist`);
     const updatedSettings = await ExtraSettings.findByIdAndUpdate(
-        space.extraSettings,   // Filter by the extraSettings id
-        updates,       // Fields to update
-        { new: true }   // Return the updated document, not the original one
+        space.extraSettings,   
+        updates,       
+        { new: true }   
     );
 
     if (!updatedSettings) {
         throw new NotFoundError(`The space ${spaceId} does not exist`);
     }
 
-    return updatedSettings; // Return the updated settings
+    return updatedSettings; 
 }
--- api/service/services/thank-you-page-service.js
+++ api/service/services/thank-you-page-service.js
@@ -7,14 +7,14 @@ export async function updateThankYouPage(spaceId, updates) {
     const space = await Space.findOne({ spaceName: spaceId });
     if (!space) throw new NotFoundError(`Space with ID ${spaceId} does not exist`);
     const updatedThankYouPage = await ThankYouPage.findByIdAndUpdate(
-         space.thankYouPage,   // Filter by spaceId
-        updates,       // Fields to update
-        { new: true }   // Return the updated document, not the original one
+         space.thankYouPage,  
+        updates,     
+        { new: true }  
     );
 
     if (!updatedThankYouPage) {
         throw new NotFoundError(`The spaceId ${spaceId} does not exist`);
     }
 
-    return updatedThankYouPage; // Return the updated settings
+    return updatedThankYouPage; 
 }
