ab1e1d8fe26241a0cb858b8cf906fdf404dd2fbb
maithwade25 <wade.mai@northeastern.edu>
2024-12-04 14:43:40 -0500

    JWT methods

--- api/service/services/user-management-service.ts
+++ api/service/services/user-management-service.ts
@@ -1,4 +1,27 @@
 import UserManage, { IUser} from '../models/user-management';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+//Secret key for JWT
+const secretKey = process.env.JWT_SECRET;
+
+//Token expiration time 
+const jwt_expiresIn = process.env.JWT_EXPIRES_IN;
+
+//Generating JWT Token 
+export const generateToken = (user: IUser): string => {
+    const payload = { id: user._id, email: user.email, username: user.username };
+    return jwt.sign(payload, secretKey, { expiresIn: jwt_expiresIn });   
+};
+
+//Verify JWT token
+export const verifyToken = (token: string): any => {
+    try {
+        return jwt.verify(token, secretKey);
+    } catch (err) {
+        throw new Error('Invalid or expired token');
+    }
+}
 
 //getting user by id
 export const getUserById = async (userId: string): Promise<Omit<IUser, "password"> | null> => {
